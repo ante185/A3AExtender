b4c9db8678d07cb3abeb55bf28922ac2a9a219b8
ante <49555217+ante185@users.noreply.github.com>
2023-09-16 00:41:03 +0200

    Install cleanup

---
+++ ws_preview/addons/core/Includes/mod_name.hpp
@@ -0,0 +1,4 @@
+#define MODFOLDER ws_preview
+#ifndef PREFIX
+    #define PREFIX ws_preview
+#endif
---
+++ ws_preview/addons/templates/script_component.hpp
@@ -0,0 +1,2 @@
+#define COMPONENT templates
+#include "\x\ws_preview\addons\core\Includes\script_mod.hpp"
