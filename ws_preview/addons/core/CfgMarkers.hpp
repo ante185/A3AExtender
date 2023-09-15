4751b00390691bbdb0118536199263f7102dd648
ante <49555217+ante185@users.noreply.github.com>
2023-09-16 01:08:58 +0200

    ion marker

---
+++ ws_preview/addons/core/CfgMarkers.hpp
@@ -0,0 +1,8 @@
+class CfgMarkers {
+	class flag_NATO;
+	class a3a_flag_ION: flag_NATO {
+		name = "ION";
+		icon = "\A3\Data_F\Flags\flag_ion_CO.paa";
+		texture = "\A3\Data_F\Flags\flag_ion_CO.paa"; //Needs unsquaring
+	};
+};
