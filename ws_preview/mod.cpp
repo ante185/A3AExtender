e11255e823bdabf296344f4b80fc95df7edc9328
ante <49555217+ante185@users.noreply.github.com>
2023-09-16 01:07:37 +0200

    Update mod.cpp

--- ws_preview/mod.cpp
+++ ws_preview/mod.cpp
@@ -1,5 +1,5 @@
-name = "A3 Antistasi Extender example";
+name = "A3 Antistasi Western Sahara Preview";
 author = "A3-Antistasi dev team";
-tooltip = "Antistasi Extender example";
-actionName = "GitHub";
-action = "https://github.com/HakonRydland/A3AExtender";
+tooltip = "Antistasi Western Sahara Preview Extention";
+actionName = "GitHub PRs";
+action = "https://github.com/official-antistasi-community/A3-Antistasi/pulls?q=is%3Aopen+is%3Apr+author%3Aante185+Western+Sahara";
