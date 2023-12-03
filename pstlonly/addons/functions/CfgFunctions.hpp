3845013c5af05fd59e1dadfec741330904127082
ante <49555217+ante185@users.noreply.github.com>
2023-12-03 20:27:02 +0100

    Add pistols to rebelGear

---
+++ pstlonly/addons/functions/CfgFunctions.hpp
@@ -0,0 +1,13 @@
+class CfgFunctions {
+    //be carefull when overwiriting functions as version updates can brake your exstension
+    class A3A {
+        class Ammunition {
+            class randomRifle {
+                file = QPATHTOFOLDER(fn_randomRifle.sqf);
+            };
+            class generateRebelGear {
+                file = QPATHTOFOLDER(fn_generateRebelGear.sqf);
+            };
+        };
+    };
+};
