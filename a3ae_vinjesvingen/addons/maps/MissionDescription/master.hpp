1d77e977b480d1eb3d90b353ee04c8a7901c73ad
ante <49555217+ante185@users.noreply.github.com>
2024-04-01 18:10:29 +0200

    Update master.hpp

--- a3ae_vinjesvingen/addons/maps/MissionDescription/master.hpp
+++ a3ae_vinjesvingen/addons/maps/MissionDescription/master.hpp
@@ -2,7 +2,7 @@
 // #include "MissionDescription\master.hpp"
 // Whether order should be maintained is unknown.
 
-author = $STR_antistasi_credits_generic_author_text;
+//author = $STR_antistasi_credits_generic_author_text;
 Keys[] = {"A3-Antistasi-is-not-available-in-single-player"};
 KeysLimit = 2;  // Even if player tampers with his unlocked keys, this will never become true.
 
