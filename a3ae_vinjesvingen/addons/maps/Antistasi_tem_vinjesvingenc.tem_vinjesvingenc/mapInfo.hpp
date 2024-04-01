c61748e8139272adbadf88b011dfdeca36e3b8a2
ante <49555217+ante185@users.noreply.github.com>
2024-04-01 23:17:49 +0200

    update

--- a3ae_vinjesvingen/addons/maps/Antistasi_tem_vinjesvingenc.tem_vinjesvingenc/mapInfo.hpp
+++ a3ae_vinjesvingen/addons/maps/Antistasi_tem_vinjesvingenc.tem_vinjesvingenc/mapInfo.hpp
@@ -14,14 +14,13 @@ class tem_vinjesvingenc {
         ,{"Myrbo",55}
         ,{"Skatter",25}
         ,{"Floten",218}
-        ,{"Campabo",45}
         ,{"Sundrisland",69}
         ,{"Autsbo",21}
         ,{"Bru",40}
         ,{"Levad",50}
         ,{"Nystog",36}
 	};
-	disabledTowns[] = {"vinjeAirfield","Northfactory","Gasstation","Factory","Sawmill","Heii","Brauti","Solhaug"};
+	disabledTowns[] = {"vinjeAirfield","Campabo","Northfactory","Gasstation","Factory","Sawmill","Heii","Brauti","Solhaug"};
 	antennas[] = {
         {5095.85,1018.74,0},{1790.87,5817.86,0.00408936}
 	};
