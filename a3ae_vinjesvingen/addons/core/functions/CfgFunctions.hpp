6b7d37a589fd65072f242a157a849032bd787dfb
ante <49555217+ante185@users.noreply.github.com>
2024-04-02 02:02:57 +0200

    adds override for fn_initZones

---
+++ a3ae_vinjesvingen/addons/core/functions/CfgFunctions.hpp
@@ -0,0 +1,14 @@
+class CfgFunctions {
+    //be carefull when overwiriting functions as version updates can brake your exstension
+    class A3A {
+        class init {
+            class initZones {
+                file = "\x\a3ae_vinjesvingen\addons\core\functions\fn_initZones.sqf";
+            };
+        };
+    };
+
+    //your own functions should be keept here
+    class ADDON {
+    };
+};
